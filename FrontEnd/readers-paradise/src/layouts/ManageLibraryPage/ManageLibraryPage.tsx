896b7cbd724eb6a9c72b2869aa2ebb04ce03778f
Utkarsh-2525 <2101641530158@psit.ac.in>
2024-07-26 01:28:15 +0530

    Change Quantity of Book interface designed

--- FrontEnd/readers-paradise/src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ FrontEnd/readers-paradise/src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 import {Redirect} from "react-router-dom";
 import {AdminMessages} from "./components/AdminMessages";
 import {AddNewBook} from "./components/AddNewBook";
+import {ChangeQuantityOfBooks} from "./components/ChangeQuantityOfBooks";
 
 export const ManageLibraryPage = () => {
 
@@ -50,7 +51,7 @@ export const ManageLibraryPage = () => {
                         <AddNewBook/>
                     </div>
                     <div className="tab-pane fade" id='nav-quantity' role='tabpanel' aria-labelledby='nav-quantity-tab'>
-                        {changeQuantityOfBooksCLick ? <>Change Quantity</> : <></>}
+                        {changeQuantityOfBooksCLick ? <ChangeQuantityOfBooks/> : <></>}
                     </div>
                     <div className="tab-pane fade" id='nav-messages' role='tabpanel' aria-labelledby='nav-messages-tab'>
                         {messagesClick ? <AdminMessages/> : <></>}
